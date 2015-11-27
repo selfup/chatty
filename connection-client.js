3f8d0a0eaf7636f8ce63fd9bf82307050ae37297
selfup <boudinot.regis@yahoo.com>
2015-11-27 16:36:45 -0700

    added user friendly chat client for the terminal

---
+++ connection-client.js
@@ -0,0 +1,12 @@
+var net = require('net')
+
+var socket = bat.connect(9000, 'localhost')
+
+socket.on('data', function (data) {
+	process.stdout.write(data);
+})
+
+process.stdin.on('data', function (data) {
+	socket.write(data)
+});
+
